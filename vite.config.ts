b8624b030115b865e7c537fcdedcdca8c3363694
Jose Castro <josehco85@gmail.com>
2025-11-27 06:40:04 -0400

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
