82ca2203410dfe7430412a8bdb2bcfce675f54e1
Jose Castro <josehco85@gmail.com>
2025-11-27 09:19:04 -0400

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,29 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        base: env.VITE_BASE_URL || '/',
-        plugins: [
-            laravel({
-                input: ['resources/js/app.ts'],
-                ssr: 'resources/js/ssr.ts',
-                refresh: true,
-                buildDirectory: 'build',
-            }),
-            tailwindcss(),
-            wayfinder({
-                formVariants: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    // Hardcoded para asegurar que funcione en producción
+    base: '/inf513/grupo23sc/proyecto2/public/',
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+            buildDirectory: 'build',
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-    };
+            },
+        }),
+    ],
 });
