29deffc8ef969938c44eab6ecb9beade8eb52cb6
Jose Castro <josehco85@gmail.com>
2025-12-17 14:39:18 -0400

    fix vite config

--- public/build/assets/CardTitle.vue_vue_type_script_setup_true_lang-Bi4FQNR6.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./AppFooter-hN-Mg31V.js";import{q as n,b as t,n as r,e as l,H as c,k as m}from"./app-Bdlr-GPS.js";const f=n({__name:"CardTitle",props:{class:{}},setup(s){const e=s;return(a,p)=>(m(),t("h3",{"data-slot":"card-title",class:r(l(o)("leading-none font-semibold",e.class))},[c(a.$slots,"default")],2))}});export{f as _};
